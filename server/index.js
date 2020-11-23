9d138422d80d3d77103f0b4961e75a808ef3c93a
Mike Solomon <mike@meeshkan.com>
2020-11-23 21:13:01 +0200

    Adds higher body parser limit

--- server/index.js
+++ server/index.js
@@ -32,45 +32,51 @@ app.post(
   }
 );
 
-app.post("/u", bodyParser.json(), function (req, res) {
-  if (!req.body.code) {
-    throw new Error("Need a code param");
-  }
-  var notPurs =
-    req.body.code.indexOf("module") < 0 ||
-    req.body.code.indexOf("main") < 0 ||
-    req.body.code.indexOf("runInBrowser") < 0;
-  var notJs =
-    req.body.code.indexOf("AudioWorkletProcessor") < 0 ||
-    req.body.code.indexOf("process") < 0 ||
-    req.body.code.indexOf("registerProcessor") < 0;
-  if (notPurs && notJs) {
-    throw new Error("invalid input");
-  }
-  mixpanel.track(notJs ? "Klank saved" : "Processor saved");
-  var stream = Buffer.from(req.body.code, "binary");
-  var o =
-    "K" +
-    new Date().getTime() +
-    "" +
-    Math.floor(Math.random() * 10000) +
-    (notPurs ? ".js" : ".purs");
-  var params = {
-    ACL: "public-read",
-    Bucket: "klank-share",
-    ContentType: notPurs ? "application/javascript" : "text/plain",
-    Key: o,
-    Body: stream,
-  };
-  s3.upload(params, function (err, data) {
-    if (err) {
-      console.log(err);
-      res.status(400).send("Server error");
-    } else {
-      res.send(data.Location);
+app.post(
+  "/u",
+  bodyParser.json({
+    limit: "10mb",
+  }),
+  function (req, res) {
+    if (!req.body.code) {
+      throw new Error("Need a code param");
     }
-  });
-});
+    var notPurs =
+      req.body.code.indexOf("module") < 0 ||
+      req.body.code.indexOf("main") < 0 ||
+      req.body.code.indexOf("runInBrowser") < 0;
+    var notJs =
+      req.body.code.indexOf("AudioWorkletProcessor") < 0 ||
+      req.body.code.indexOf("process") < 0 ||
+      req.body.code.indexOf("registerProcessor") < 0;
+    if (notPurs && notJs) {
+      throw new Error("invalid input");
+    }
+    mixpanel.track(notJs ? "Klank saved" : "Processor saved");
+    var stream = Buffer.from(req.body.code, "binary");
+    var o =
+      "K" +
+      new Date().getTime() +
+      "" +
+      Math.floor(Math.random() * 10000) +
+      (notPurs ? ".js" : ".purs");
+    var params = {
+      ACL: "public-read",
+      Bucket: "klank-share",
+      ContentType: notPurs ? "application/javascript" : "text/plain",
+      Key: o,
+      Body: stream,
+    };
+    s3.upload(params, function (err, data) {
+      if (err) {
+        console.log(err);
+        res.status(400).send("Server error");
+      } else {
+        res.send(data.Location);
+      }
+    });
+  }
+);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server started");
