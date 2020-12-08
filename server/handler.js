0c56a1786656804418ab42abb9bd0c09d1feff6a
Mike Solomon <mike@meeshkan.com>
2020-12-08 17:39:22 +0200

    Adds handler

---
+++ server/handler.js
@@ -0,0 +1,30 @@
+var ps = require("./output/Main/");
+var Mixpanel = require("mixpanel");
+
+var mixpanel = Mixpanel.init(process.env.MIXPANEL_TOKEN, {
+  host: "api-eu.mixpanel.com",
+});
+
+exports.handler = function(event, context, callback) {
+    if (!event.body) {
+      throw new Error("Need a body.");
+    }
+    var tops = JSON.parse(event.body);
+    if (!tops.code) {
+      throw new Error("Need code.");
+    }
+    mixpanel.track("Klank compiled");
+    ps.compile({body: tops})(function (r) {
+      return function () {
+          console.log("successful completion");
+            callback(null,{
+                "statusCode": 200,
+                "headers": {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Headers": "Content-Type, X-Amz-Date, Authorization, X-Api-Key, X-Amz-Security-Token, X-Amz-User-Agent",
+                },
+                "body": JSON.parse(r),
+            });
+      };
+    })();
+  }
