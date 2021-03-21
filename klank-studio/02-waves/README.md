9b868e67b4dedea4a8ed09fb524ced3b785b4040
Mike Solomon <mike@meeshkan.com>
2021-03-21 08:55:05 +0200

    Adds waves

---
+++ klank-studio/02-waves/README.md
@@ -0,0 +1,17 @@
+# klank-webapp
+
+## To run in studio mode
+
+In studio mode, your klank lives in the `./studio` folder. When you save your klank as you're working on it, the site will hot-reload.
+
+```bash
+npm install
+npm run studio
+```
+
+## To run the klank.dev site locally
+
+```bash
+npm install
+npm run serve
+```
---
+++ klank-studio/02-waves/purs/Main.purs
@@ -0,0 +1,35 @@
+module Main where
+
+import Prelude
+import Effect (Effect)
+import Halogen.Aff as HA
+import Halogen.VDom.Driver (runUI)
+import Data.List ((:), List(..))
+import Data.NonEmpty ((:|))
+import Data.Typelevel.Num (D1)
+import FRP.Behavior.Audio (AudioUnit, gain', runInBrowser, sinOsc, speaker)
+import Math (pi, sin, cos)
+import Type.Klank.Dev (Klank, klank)
+import Klank.Weblib.Studio as Studio
+
+scene :: Number -> AudioUnit D1
+scene time =
+  speaker
+    ( (gain' (0.1 + (-0.1) * cos (0.5 * rad)) (sinOsc $ 440.0 + 6.0 * (sin (0.1 * rad))))
+        :| (gain' (0.1 + (-0.1) * cos (0.47 * rad)) (sinOsc $ 330.0 + 4.0 * (sin (0.2 * rad))))
+        : Nil
+    )
+  where
+  rad = pi * time
+
+playMe :: Klank
+playMe =
+  klank
+    { run = runInBrowser scene
+    }
+
+main :: Effect Unit
+main =
+  HA.runHalogenAff do
+    body <- HA.awaitBody
+    runUI (Studio.component (pure playMe)) unit body
